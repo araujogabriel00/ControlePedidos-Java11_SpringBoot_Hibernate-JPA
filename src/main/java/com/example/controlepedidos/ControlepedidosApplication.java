0a9a165910a5a5f1b23f81d49aa36a9dd38a0730
Gabriel Araujo <gabriel.araujos@sempreceub.com>
2020-11-28 05:45:50 -0300

    Project Created

---
+++ src/main/java/com/example/controlepedidos/ControlepedidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlepedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlepedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controlepedidos/ControlepedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlepedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
